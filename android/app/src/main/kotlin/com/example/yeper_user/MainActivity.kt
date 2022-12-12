a82664f5cc88b0b8d8e468b05fa6d41fed52e6fe
sanjay <sanjaykumar73189@gmail.com>
2022-12-12 22:30:58 +0530

    Home Page Working

---
+++ android/app/src/main/kotlin/com/example/yeper_user/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yeper_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:yeper_user/Screens/HomeScreen/HomeScreen.dart';
+import 'package:yeper_user/constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Yeper Client",
+      theme: ThemeData(
+        scaffoldBackgroundColor: kbackgroundColor,
+        primaryColor: kprimarycolor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
