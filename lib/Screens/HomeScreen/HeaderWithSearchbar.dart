56b31bc1fcf344f107cc19ef999816be2d15288c
sanjay <sanjaykumar73189@gmail.com>
2022-12-12 22:53:35 +0530

    Header with search bar

---
+++ lib/Screens/HomeScreen/HeaderWithSearchbar.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../constants.dart';
+class HeaderWithSearchbar extends StatelessWidget {
+  const HeaderWithSearchbar({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      height: size.height * 0.2,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * 0.2 - 27,
+            decoration: BoxDecoration(
+                color: kprimarycolor,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(36),
+                    bottomRight: Radius.circular(36))),
+            child: GestureDetector(
+              onTap: (() {
+                print("Hello");
+              }),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "Your Balance",
+                      style: Theme.of(context).textTheme.headline5?.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.w300),
+                    ),
+                    Text(
+                      "\$ 24",
+                      style: Theme.of(context).textTheme.headline5?.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                height: 54,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kprimarycolor.withOpacity(0.23)),
+                  ],
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          hintText: "Search Card",
+                          hintStyle:
+                              TextStyle(color: kprimarycolor.withOpacity(0.5)),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    SvgPicture.asset("assets/icons/search.svg")
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
