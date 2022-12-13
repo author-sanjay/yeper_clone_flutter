9e636178040a9a32625b08969b1e94b9385f8a72
sanjay <sanjaykumar73189@gmail.com>
2022-12-13 20:08:21 +0530

    Card

---
+++ lib/Screens/Referal/Referal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Referral extends StatelessWidget {
+  const Referral({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
