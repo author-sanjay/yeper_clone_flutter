b14e6220d385b08504675b27cf48413bf8dd6a3e
sanjay <sanjaykumar73189@gmail.com>
2022-12-13 13:03:28 +0530

    Details Page

--- lib/Screens/DetailsScreen/Components/Body.dart
+++ lib/Screens/DetailsScreen/Components/Body.dart
@@ -1,30 +1,59 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:yeper_user/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
-    Size size= MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return Column(
       children: <Widget>[
-        Row(
-          children: <Widget>[
-            Expanded(
-              child: Column() 
-            ),
-            Container( height: size.height*0.8,
-            width: size.width*0.75,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                alignment: Alignment.centerLeft,
-                fit: BoxFit.cover,
-                image: AssetImage("assets/images/img.png"))
-            ),
-            )
-          ],
+        SizedBox(
+          height: size.height*0.8,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: kDefaultPadding*3),
+                    child: Column(
+                children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
+                      ),
+                    )
+                ],
+              ),
+                  )),
+              Container(
+                height: size.height * 0.8,
+                width: size.width * 0.75,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(63),
+                        bottomLeft: Radius.circular(63)),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 60,
+                          color: kprimarycolor.withOpacity(0.29)),
+                    ],
+                    image: DecorationImage(
+                        alignment: Alignment.centerLeft,
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/images/img.png"))),
+              )
+            ],
+          ),
         )
       ],
     );
