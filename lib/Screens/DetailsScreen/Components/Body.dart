b4f99e1a5e9811d24dea1558f5ba493036e9b1c8
sanjay <sanjaykumar73189@gmail.com>
2023-01-03 01:45:15 +0530

    update

--- lib/Screens/DetailsScreen/Components/Body.dart
+++ lib/Screens/DetailsScreen/Components/Body.dart
@@ -30,20 +30,69 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            color: kprimarycolor,
-            height: MediaQuery.of(context).size.height * 0.1,
-            child: Row(children: [
-              IconButton(onPressed: (){}, icon: Icon(Icons.navigate_before, size: 40,color: Colors.white,)),
-              Spacer(),
-              Container(child: Text("Details", style: TextStyle(color: Colors.white),)),
-              Spacer(),
-            ]),
-          )
-        ],
+      child: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              headerwithname(),
+              SizedBox(
+                height: 10,
+              ),
+              imageofproduct(),
+              
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class imageofproduct extends StatelessWidget {
+  const imageofproduct({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.3,
+      width: MediaQuery.of(context).size.width,
+      // color: Colors.cyan,
+      child: Image(
+          image: AssetImage("assets/images/image (1).jpg"),
+          fit: BoxFit.cover),
+    );
+  }
+}
+
+class headerwithname extends StatelessWidget {
+  const headerwithname({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: kprimarycolor,
+      height: MediaQuery.of(context).size.height * 0.08,
+      child: Row(children: [
+        IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.navigate_before,
+              size: 40,
+              color: Colors.white,
+            )),
+        Spacer(),
+        Text(
+          "Details",
+          style: TextStyle(
+              color: Colors.white, fontSize: 30, fontWeight: FontWeight.w300),
+        ),
+        Spacer(),
+      ]),
     );
   }
 }
